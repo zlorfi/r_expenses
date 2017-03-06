791295c7ebc2b955148056c2f70f4ebc33917587
Michael Skrynski <michael.skrynski@dkd.de>
2017-03-06 11:28:31 +0100

    show active class despite any params

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
   end
 
   def nav_link(text, path)
-    link_class = current_page?(path) ? "nav-item nav-link active" : "nav-item nav-link"
+    link_class = current_page?(path.split('?').first) ? "nav-item nav-link active" : "nav-item nav-link"
     link_to text, path, class: link_class
   end
 
