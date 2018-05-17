d8f8735b747ad80ce7d61f7347daf6d0e0154a59
Michael Skrynski <zlorfi@gmail.com>
2018-05-17 11:58:14 +0200

    setting correct content_type

--- app/controllers/concerns/auth.rb
+++ app/controllers/concerns/auth.rb
@@ -7,7 +7,7 @@ module Auth
 
     rescue_from CanCan::AccessDenied do |exception|
       respond_to do |format|
-        format.json { head :forbidden, content_type: 'text/html' }
+        format.json { head :forbidden, content_type: 'application/json' }
         format.html { redirect_to root_url, alert: exception.message }
         format.js   { head :forbidden, content_type: 'text/html' }
       end
