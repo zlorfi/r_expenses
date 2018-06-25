6a3482f47d7edc886a1336a9dc08f2ae0ece7960
Michael Skrynski <zlorfi@gmail.com>
2018-06-25 15:53:25 +0200

    readded rails-ujs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,6 +5,7 @@
 // or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
 //
 //= require jquery-3.3.1.slim
+//= require rails-ujs
 //= require bootstrap
 //= require turbolinks
 //= require expenses_jquery
