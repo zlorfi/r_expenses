490c82ab8bad7a179462311a0c9cfef8c84473a8
Michael Skrynski <zlorfi@gmail.com>
2019-12-03 20:24:59 +0100

    adding correct localization for d3

--- app/webpacker/shared/charts.js
+++ app/webpacker/shared/charts.js
@@ -1,10 +1,6 @@
 import * as d3 from 'd3';
 
 d3.timeFormatDefaultLocale({
-  'decimal': ',',
-  'thousands': '.',
-  'grouping': [3],
-  'currency': ['', ' €'],
   'dateTime': '%a %b %e %X %Y',
   'date': '%d.%m.%Y',
   'time': '%H:%M:%S',
@@ -13,4 +9,11 @@ d3.timeFormatDefaultLocale({
   'shortDays': ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
   'months': ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
   'shortMonths': ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez']
+});
+
+d3.formatDefaultLocale({
+  'decimal': ',',
+  'thousands': '.',
+  'grouping': [3],
+  'currency': ['', ' €']
 });
